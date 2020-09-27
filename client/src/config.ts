f40c1f323426fbd914dd33bd4510d4f732abd36f
Heliton Bruning <heliton3304@gmail.com>
2020-09-27 14:30:23 -0300

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'wccurwlu0a'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'nakaloumenos.auth0.com', // Auth0 domain
-  clientId: 'EfI2dI9xxADH2DAhXrHNCQozAy1vCP5B', // Auth0 client id
+  domain: 'dev-k5hu8rdu.us.auth0.com', // Auth0 domain
+  clientId: 'aGI7VDqz7ppBJ4OglXc3IktjnvCGeoQX', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
